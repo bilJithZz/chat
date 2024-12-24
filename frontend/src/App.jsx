26c4aa350d84c7abfbb6944a2ad48723fabbde37
biljithz <biljithjoseph@gmail.com>
2024-12-24 14:17:52 +0530

    adding ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,37 @@
+import React from 'react';
+import './App.css';
+import { Button } from './components/ui/button';
+import { Input } from "@/components/ui/input"
+import { useToast } from "@/hooks/use-toast"
+import { Toggle } from "@/components/ui/toggle"
+import { Textarea } from "@/components/ui/textarea"
+import { Toaster } from "@/components/ui/toaster"
 
-import './App.css'
-import { Button } from './components/ui/button'
+
 
 function App() {
 
+  const { toast } = useToast()
+
   return (
-    <>
-     <Button>hello</Button>
-    </>
-  )
+    <div className=" bg-red-800 w-screen  h-screen flex">
+     <div className="flex  max-w-sm  space-x-2">
+     <Textarea className="h-10 rounded-lg" placeholder="Type your message here." >
+    
+     </Textarea>
+     <Button  onClick={() => {
+        toast({
+          title: "Scheduled: Catch up",
+          description: "Friday, February 10, 2023 at 5:57 PM",
+        })
+      }}  type="submit">Subscribe</Button>
+     
+      <Toggle>Toggle</Toggle>
+
+      <Toaster />
+    </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/layout.jsx
@@ -0,0 +1,15 @@
+import { Toaster } from "@/components/ui/toaster";
+
+export default function RootLayout() {
+  return (
+    <html lang="en">
+      <head />
+      <body>
+        <main>
+        <Toaster />
+        </main>
+      
+      </body>
+    </html>
+  );
+}
